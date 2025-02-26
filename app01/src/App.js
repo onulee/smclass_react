333953033e96ffdb631126c38fc2e30f5d442e22
sm <onulee@naver.com>
2025-02-26 12:15:05 +0900

    react수업1

---
+++ app01/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import './css/style.css'; //주석: css파일 import
+
+function App() {
+  const css01 = {
+    textAlign:'center',
+    
+  }
+
+  return (
+    <div>
+      <h2>로그인</h2>
+      <div style={css01} className="d01">aaa</div>
+      <button>확인</button>
+    </div>
+  );
+}
+
+export default App;
